f4c8db4dcada842f43785e376a8ded48eb1913a5
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-08-31 02:51:40 +0600

    move images to advisors

--- src/services/AdvisorsService.jsx
+++ src/services/AdvisorsService.jsx
@@ -5,32 +5,32 @@ const AdvisorsService = () => {
     {
       name: "অধ্যাপক ড. মোঃ আখতারুজ্জামান",
       designation: "মাননীয় উপাচার্য, ঢাকা বিশ্ববিদ্যালয়",
-      image: "akhtaruzzaman.png",
+      image: "advisors/akhtaruzzaman.png",
     },
     {
       name: "হাফিজুর রহমান রিয়েল ",
       designation: "উপ-পুলিশ কমিশনার, ট্রাফিক-গুলশান বিভাগ",
-      image: "hafizur-rahman.png",
+      image: "advisors/hafizur-rahman.png",
     },
     {
       name: "এবিএম আসাদুজ্জামান",
       designation: "ভারপ্রাপ্ত কর্মকর্তা, ভাটারা থানা",
-      image: "asaduzzaman.png",
+      image: "advisors/asaduzzaman.png",
     },
     {
       name: "কাজী শাহান হক",
       designation: "ভারপ্রাপ্ত কর্মকর্তা, খিলক্ষেত থানা",
-      image: "kazi-shahin.png",
+      image: "advisors/kazi-shahin.png",
     },
       {
         name: "হাবিবুর রহমান জুয়েল",
         designation: "প্রতিষ্ঠাতা সভাপতি, ফাল্গুনী বাস রুট",
-        image: "habibur-rahman.png",
+        image: "advisors/habibur-rahman.png",
       },
       {
         name: "কামরুল হাসান সেতু",
         designation: "সাবেক সভাপতি, ফাল্গুনী বাস রুট",
-        image: "kamrul-hasan.png",
+        image: "advisors/kamrul-hasan.png",
       },
   ];
 }
