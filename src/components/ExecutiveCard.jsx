d91e8c67c927329cf09f505253d479b36536d838
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2023-08-31 10:06:39 +0600

    fix ring border

--- src/components/ExecutiveCard.jsx
+++ src/components/ExecutiveCard.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 const ExecutiveCard = ({executive}) => {
   return (
-    <div class="group ease-in-out transition w-36 lg:w-64 mx-4 my-2 p-1 lg:p-8 bg-white hover:bg-[#6251A7] ring-4 ring-[#6251A7]  rounded-xl shadow-md transform hover:scale-105 duration-500">
+    <div class="group ease-in-out transition w-36 lg:w-64 mx-4 my-2 p-1 lg:p-8 bg-white hover:bg-[#6251A7] lg:ring-4 ring-2 ring-[#6251A7]  rounded-xl shadow-md transform hover:scale-105 duration-500">
         <img
-          class="h-12 w-12 lg:h-24 lg:w-24 mx-auto rounded-full ring-4 ring-[#6251A7] group-hover:ring-[#F8DE22] mt-3 lg:mt-7"
+          class="h-12 w-12 lg:h-24 lg:w-24 mx-auto rounded-full lg:ring-4 ring-2 ring-[#6251A7] group-hover:ring-[#F8DE22] mt-3 lg:mt-7"
           src={executive.image}
           alt="Somoyon Executives"
         />
-              {/* <img class="h-24 mx-auto rounded-full ring-4 ring-green-400 sm:mx-0 sm:flex-shrink-0 transform hover:scale-105 duration-500" src="/images/logo.png" alt="Learn with Sumit logo" /> */}
 
         <div class="text-center space-y-2 my-5 lg:my-1">
           <div class="space-y-0.5">
